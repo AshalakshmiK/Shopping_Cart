14d2382fb019bdae2bbbcc13ef2ffd609e03e7cd
AshalakshmiK <ashakakunje@gmail.com>
2020-03-28 01:37:48 +0530

    changes to server.js

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-var path = require('path');
-var express = require('express');
+const path = require('path');
+const express = require('express');
+const port=process.env.PORT || 8080
+const app = express();
 
-var app = express();
+// app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8080);
+app.use(express.static(__dirname,'dist'))
 
-var server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
-});
+app.use('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'index.html'))
+})
+//app.set('port', process.env.PORT || 8080);
+
+// var server = app.listen(app.get('port'), function() {
+//   console.log('listening on port ', server.address().port);
+// });
+
+app.listen(port);
+console.log("server started")
